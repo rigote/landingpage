06f43a9c9070341c46b1c9307b1a26344371e667
Matheus Rigote <matheus.rigote@gmail.com>
2020-11-18 00:49:52 -0300

    Graphql config + Logo component

---
+++ src/graphql/queries/getLandingPage.ts
@@ -0,0 +1,11 @@
+const GET_LANDING_PAGE = /* Graphql */ `
+  query GET_LANDING_PAGE {
+    landingPage{
+      logo{
+        alternativeText
+        url
+      }
+    }
+  }
+`
+export default GET_LANDING_PAGE
